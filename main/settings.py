b697183b0da4d241ff6a47e325691f2310662c82
esadakman <esadd26@gmail.com>
2022-09-07 20:03:44 +0300

    styling

--- main/settings.py
+++ main/settings.py
@@ -22,7 +22,7 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
-    "django.contrib.staticfiles",
+    "django.contrib.staticfiles", 
     # my Apps
     'todo',
     'crispy_forms',
